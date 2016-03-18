2588db122d0eee8646e48eb637703f77388d7cf9
Richard Price <mrspondooli@gmail.com>
2016-03-18 06:28:16 +0000

    Initial Commit

---
+++ pokeman-pokedex/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokeman-pokedex
+//
+//  Created by Richard Price on 18/03/2016.
+//  Copyright © 2016 twisted echo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
