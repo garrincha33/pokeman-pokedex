6b96454f558ddd70edb0e485370dcfd580f023cc
Richard Price <mrspondooli@gmail.com>
2016-04-11 22:21:36 +0100

    finished PokemonVC, tested talking to API pulling down data. All working. Finished

--- pokeman-pokedex/PokemonDetailVC.swift
+++ pokeman-pokedex/PokemonDetailVC.swift
@@ -22,20 +22,59 @@ class PokemonDetailVC: UIViewController {
     @IBOutlet weak var currentEvoImg: UIImageView!
     @IBOutlet weak var nextEvoImg: UIImageView!
     @IBOutlet weak var evoLbl: UILabel!
+    @IBOutlet weak var weightLbl: UILabel!
+    @IBOutlet weak var baseAttackLbl: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        nameLbl.text = pokemon.name
-        mainImg.image = UIImage(named: "\(pokemon.pokemonId)")
+        nameLbl.text = pokemon.name.capitalizedString
+        let img = UIImage(named: "\(pokemon.pokemonId)")
+        mainImg.image = img
+        currentEvoImg.image = img
         
         pokemon.downloadPokemonDetails {
             
             () -> () in
             
-            
+            self.updateUI()
+ 
         }
  
+    }
+    
+    func updateUI() {
+        
+      descriptionLbl.text = pokemon.description
+      typeLbl.text = pokemon.type
+      defenseLbl.text = pokemon.defense
+      heightLbl.text = pokemon.height
+      weightLbl.text = pokemon.weight
+      baseAttackLbl.text = pokemon.attack
+      pokedexLbl.text = "\(pokemon.pokemonId)"
+        
+        if pokemon.nextEvolutionId == "" {
+            
+            evoLbl.text = "No Evolutions"
+            nextEvoImg.hidden = true
+            
+        } else {
+            
+            nextEvoImg.hidden = false
+            nextEvoImg.image = UIImage(named: pokemon.nextEvolutionId)
+            var str = "Next Evolution: \(pokemon.nextEvolutiontxt)"
+            
+            if pokemon.nextEvolutionLvl != "" {
+                
+                str += " - LVL \(pokemon.nextEvolutionLvl)"
+                
+                evoLbl.text = str
+                
+            }
+            
+        }
+
     }
 
     
